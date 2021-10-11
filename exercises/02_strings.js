b3d079dc3aebc14b24897cdf66a9872bcaa288f3
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:21:56 -0300

    added the third item of the exercise

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -12,3 +12,12 @@ primeros 5 caracteres guardando el resultado en una nueva variable (utilizar sub
 
 var newStr = lowToUp , subStr;
 console.log(subStr = newStr.substring(0, 5));
+
+/*
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los 
+últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
+
+var newStr2 = lowToUp , subStr2, len;
+len = lowToUp.length;
+console.log(subStr2 = newStr2.substring(len-3 ,));
