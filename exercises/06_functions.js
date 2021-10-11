bc1fb5332c300a1cd093b625ca7826c7d29cbdff
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:38:43 -0300

    created 06_functions.js and added the first item of the exercise

---
+++ exercises/06_functions.js
@@ -0,0 +1,12 @@
+/*
+Crear una función suma que reciba dos valores numéricos y retorne el resultado. 
+Ejecutar la función y guardar el resultado en una variable, mostrando el valor de 
+dicha variable en la consola del navegador.
+*/
+
+function addition(val1 , val2){
+    return val1 + val2;
+}
+
+var numb1 = 1,  numb2 = 7 , numb3;
+console.log(numb3 = addition(numb1, numb2));
