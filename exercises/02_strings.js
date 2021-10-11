58712d23b7e86cd2d977cef93fb700e43b4f9633
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:19:13 -0300

    created 02_strings.js, added first item of the exercise

---
+++ exercises/02_strings.js
@@ -0,0 +1,6 @@
+/*
+Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+*/
+
+var lowToUp = 'ricardofort';
+console.log(lowToUp.toUpperCase());
