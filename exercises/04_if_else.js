dd33e986b680eb08a131ae0dddf1c450ee72ffc9
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:34:03 -0300

    added second and final item of the exercise

--- exercises/04_if_else.js
+++ exercises/04_if_else.js
@@ -11,3 +11,37 @@ if(ran < 0.5){
 else{
     console.log('Greater than 0.5')
 }
+
+/*
+Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+“Bebe” si la edad es menor a 2 años;
+“Nino” si la edad es entre 2 y 12 años;
+“Adolescente” entre 13 y 19 años;
+“Joven” entre 20 y 30 años;
+“Adulto” entre 31 y 60 años;
+“Adulto mayor” entre 61 y 75 años;
+“Anciano” si es mayor a 75 años.
+*/
+
+var age = Math.floor(Math.random() * 101);
+if(age < 2){
+    console.log('Bebe');
+}
+else if(age >= 2 && age <= 12){
+    console.log('Nino');
+}
+else if(age >= 13 && age <= 19){
+    console.log('Adolescente');
+}
+else if(age >= 20 && age <= 30){
+    console.log('Joven');    
+}
+else if(age >= 31 && age <= 60){
+    console.log('Adulto');    
+}
+else if(age >= 61 && age <= 75){
+    console.log('Adulto mayor');    
+}
+else if(age > 75){
+    console.log('Anciano');    
+}
