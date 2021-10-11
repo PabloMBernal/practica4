e7e409ff42b5b45855b8ccd4da6314db00a6cdcc
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:22:46 -0300

    added the fourth item of the exercise

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -21,3 +21,12 @@ Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
 var newStr2 = lowToUp , subStr2, len;
 len = lowToUp.length;
 console.log(subStr2 = newStr2.substring(len-3 ,));
+
+/*
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la 
+primera letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva 
+variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+*/
+
+var newStr3 = lowToUp , capLetter;
+console.log(capLetter = newStr3.substring(0,1).toUpperCase() + newStr3.substring(1,).toLowerCase());
