1f723d26bb6f1c2c4812d3fcee61d493308e7bc0
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:34:57 -0300

    created 05_for.js and added the first item of the exercise

---
+++ exercises/05_for.js
@@ -0,0 +1,10 @@
+/*
+Crear un array que contenga 5 palabras y recorrer dicho array utilizando 
+un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+*/
+
+var pentArray = ['gorra' , 'campera' , 'buzo' , 'pantalon' , 'medias'];
+
+for(var i = 0; i < pentArray.length; i++){
+    console.log(pentArray[i]);
+}
