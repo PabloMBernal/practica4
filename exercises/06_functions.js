8ace95cd4c1e67cac381d0c303738e08b46797da
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:39:34 -0300

    added second item of the exercise

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -10,3 +10,25 @@ function addition(val1 , val2){
 
 var numb1 = 1,  numb2 = 7 , numb3;
 console.log(numb3 = addition(numb1, numb2));
+
+/*
+A la función suma anterior, agregarle una validación para controlar si alguno de los 
+parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros 
+tiene error y retornar el valor NaN como resultado.
+*/
+
+function addition(val1 , val2){
+    if (isNaN(val1) || isNaN(val2)){
+        if(isNaN(val1)){
+            console.log('the first element is not a number')
+        }
+        if(isNaN(val2)){
+            console.log('the second element is not a number');
+        }
+        return NaN;
+    }
+    return val1 + val2;
+}
+
+var numb1 = 1,  numb2 = 'd' , numb3;
+console.log(numb3 = addition(numb1, numb2));
