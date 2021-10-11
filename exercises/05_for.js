0d900f57a20b5efe239b972b1c83a6293f8258c9
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:36:44 -0300

    added the third item of the exercise

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -16,3 +16,15 @@ Al array anterior convertir la primera letra de cada palabra en mayúscula y mos
 for(var i = 0; i < pentArray.length; i++){
     console.log(pentArray[i].substring(0,1).toUpperCase() + pentArray[i].substring(1,).toLowerCase());
 }
+
+/*
+Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un 
+bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con 
+la cadena completa.
+*/
+
+var sentence = '';
+for(var i = 0; i < pentArray.length; i++){
+    sentence += pentArray[i];
+}
+console.log(sentence);
