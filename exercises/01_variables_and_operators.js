3031872db5129aa9650b91c9ec49380780ad2fc4
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:15:08 -0300

    first commit, first item of first exercise and index.html

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,8 @@
+/*
+Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+*/
+
+var numb_1, numb_2, result;
+numb_1 = 15;
+numb_2 = 10;
+console.log(result = numb_1 + numb_2);
