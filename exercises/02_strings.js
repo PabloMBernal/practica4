f2e553c0e77c08d53ec8158ea4376f6d5bb00900
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-11 12:20:11 -0300

    added second item of the exercise

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -4,3 +4,11 @@ Crear una variable de tipo string con al menos 10 caracteres y convertir todo el
 
 var lowToUp = 'ricardofort';
 console.log(lowToUp.toUpperCase());
+
+/*
+Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los 
+primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+*/
+
+var newStr = lowToUp , subStr;
+console.log(subStr = newStr.substring(0, 5));
